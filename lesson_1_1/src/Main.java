6a85f541a5abe3ef53286994b868f1dbf7a6a066
aliyashka1 <aliyaimanbek2023@gmail.com>
2023-04-04 16:43:17 +0600

    Initial commit

---
+++ lesson_1_1/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет мир!");
+        System.out.println(5 + 4 * 3 / 2 - 1);
+
+        System.out.println(5 + 4 * 3 / (2 - 1));
+        System.out.println(7 % 3);
+
+        System.out.println("Hello " + "JAVA");
+        System.out.println("Result: " + 4 + 5);
+        System.out.println("Result: " + (4 + 5));
+        // Hello from your Sensei
+
+        int ageOfMyFriend = 25; // Объявление и инициализация переменной
+
+        System.out.println("Возраст моего друга: " + ageOfMyFriend);
+
+        ageOfMyFriend = 30;
+        System.out.println("Возраст моего нового друга: " + ageOfMyFriend);
+
+        int apples;// Объявление переменной
+
+        System.out.println("Пошли в сад и нашли яблоки");
+        apples = 65; // инициализация переменной
+
+        boolean isRainy = true;
+
+        double digit = 67.456;
+
+        String myName = "Andrei";
+
+    }
+}
